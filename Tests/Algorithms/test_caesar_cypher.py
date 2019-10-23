a46e9c4421cbc5677c9f83405afba344b57f9aa0
Otto <otto@masterbranch.io>
2019-10-23 14:58:42 +0100

    new tests added

--- Tests/Algorithms/test_caesar_cypher.py
+++ Tests/Algorithms/test_caesar_cypher.py
@@ -1,5 +1,6 @@
 import unittest
 import Algorithms
+import string
 
 
 class CaesarCypherTests(unittest.TestCase):
@@ -14,3 +15,27 @@ class CaesarCypherTests(unittest.TestCase):
         shift = 13
         encoded_text = Algorithms.caesar_cypher.encode(plain_text, shift)
         self.assertEqual('vslbhnerbagurpragranelgenvyxrrcfbyivatgurpunyyratrf', encoded_text)
+
+    def test_encode_works_with_empty_text(self):
+        plain_text = ''
+        shift = 1
+        encoded_text = Algorithms.caesar_cypher.encode(plain_text, shift)
+        self.assertEqual('', encoded_text)
+
+    def test_decode_works_with_empty_text(self):
+        encoded_text = ''
+        shift = 1
+        decoded_text = Algorithms.caesar_cypher.decode(encoded_text, shift)
+        self.assertEqual('', decoded_text)
+
+    def test_decode_returns_original_text_on_zero_shift(self):
+        encoded_text = string.ascii_lowercase
+        shift = 0
+        decoded_text = Algorithms.caesar_cypher.decode(encoded_text, shift)
+        self.assertEqual(string.ascii_lowercase, decoded_text)
+
+    def test_encode_returns_original_text_on_zero_shift(self):
+        plain_text = string.ascii_lowercase
+        shift = 0
+        encoded_text = Algorithms.caesar_cypher.encode(plain_text, shift)
+        self.assertEqual(string.ascii_lowercase, encoded_text)
