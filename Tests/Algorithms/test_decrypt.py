a30c857c4f035f4c820d97d95660de5b4e3f936c
Otto <otto@masterbranch.io>
2019-10-25 19:23:33 +0100

    fixing test failure on generator

--- Tests/Algorithms/test_decrypt.py
+++ Tests/Algorithms/test_decrypt.py
@@ -7,7 +7,7 @@ class EncryptTests(unittest.TestCase):
     def test_exhaust_finds_breaks_the_cipher(self):
         plain_text = 'BTEGRXFS'
         alphabet = string.ascii_uppercase
-        candidates = Algorithms.exhaust(plain_text, alphabet)
+        candidates = list(Algorithms.exhaust(plain_text, alphabet))
         self.assertEqual(len(alphabet), len(candidates))
         self.assertIn('ASDFQWER', candidates)
         self.assertEqual('ASDFQWER', candidates[25])
