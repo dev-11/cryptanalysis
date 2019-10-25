30a569e7cbdc4b75458af7ae3f659b99d2f8f58a
Otto <otto@masterbranch.io>
2019-10-25 19:17:42 +0100

    using yield in exhaust

--- Algorithms/decrypt.py
+++ Algorithms/decrypt.py
@@ -4,7 +4,5 @@ from Formatters import formatters
 
 def exhaust(cipher, alphabet):
     merged_cipher = formatters.merge_cipher_blocks(cipher)
-    candidates = []
     for shift in range(len(alphabet)):
-        candidates.append(caesar_cypher.decode(merged_cipher, shift, alphabet))
-    return candidates
+        yield caesar_cypher.decode(merged_cipher, shift, alphabet)
