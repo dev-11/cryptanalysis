5cbbac78f249bf533905eb6955d80e57b91f860c
Otto <otto@masterbranch.io>
2019-10-26 23:35:24 +0100

    adding decrypt to keyword cipher

--- Algorithms/keyword_cipher.py
+++ Algorithms/keyword_cipher.py
@@ -3,6 +3,11 @@ class KeywordCipher:
         key = self.create_key(alphabet, keyword)
         return str.translate(plain_text, str.maketrans(alphabet, key))
 
-    def create_key(self, alphabet, keyword):
+    def decrypt(self, cipher_text, alphabet, keyword):
+        key = self.create_key(alphabet, keyword)
+        return str.translate(cipher_text, str.maketrans(key, alphabet))
+
+    @staticmethod
+    def create_key(alphabet, keyword):
         keyword_without_duplicates = ''.join(sorted(set(keyword), key=keyword.index))
         return keyword_without_duplicates + ''.join([x for x in alphabet if x not in keyword_without_duplicates])
--- Tests/Algorithms/test_keyword_cipher.py
+++ Tests/Algorithms/test_keyword_cipher.py
@@ -10,6 +10,12 @@ class KeywordCipherTests(unittest.TestCase):
         cipher = KeywordCipher().encrypt(plain_text, string.ascii_uppercase, keyword)
         self.assertEqual('OTIPRTPC ZM NUC BCX NA NUC UCTJN AQ DCADOC', cipher)
 
+    def test_keyword_cipher_returns_correct_cipher(self):
+        plain_text = 'OTIPRTPC ZM NUC BCX NA NUC UCTJN AQ DCADOC'
+        keyword = 'THEGCHQPUZZLEBOOKII'
+        decrypted_text = KeywordCipher().decrypt(plain_text, string.ascii_uppercase, keyword)
+        self.assertEqual('LANGUAGE IS THE KEY TO THE HEART OF PEOPLE', decrypted_text)
+
     def test_create_key_creates_key_from_alphabet_and_keyword(self):
         keyword = 'THEGCHQPUZZLEBOOKII'
         alphabet = string.ascii_uppercase
