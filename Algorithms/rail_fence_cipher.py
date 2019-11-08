1cf342c14b94bdf1aa0ee46ef388ebf7d39a9252
Otto <otto@masterbranch.io>
2019-11-08 21:05:33 +0000

    Reformatting code

--- Algorithms/rail_fence_cipher.py
+++ Algorithms/rail_fence_cipher.py
@@ -3,6 +3,6 @@ class RailFenceCipher:
         return ''.join(plain_text[index] for index in range(len(plain_text)))
 
     def decrypt(self, encrypted_text, key):
-        return ''.join(encrypted_text[index] for index in range(len(encrypted_text)))
-
+        return ''.join(encrypted_text[index]
+                       for index in range(len(encrypted_text)))
 
